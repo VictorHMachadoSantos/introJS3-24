3451a909ae2c5fff85d4c45ccc2b32281ede1f93
Victor  Hugo Machado Santos <126088177+VictorHMachadoSantos@users.noreply.github.com>
2024-03-15 22:10:15 -0300

    Add files via upload

---
+++ introJS3-24-main/introJS3-24-main/2_string/kaique cupim.dart
@@ -0,0 +1,16 @@
+const text1 = 'Kaike cupin de aço';
+const text2 = 'gabizin';
+
+const texto3 = 'O gabizin foi no "Banheiro"';
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
+   
+console.log(texto2 + texto3);
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
