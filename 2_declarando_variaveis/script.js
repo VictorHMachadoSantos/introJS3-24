de8ce3b4b5c95b99337c88d2ccecc297adaa03f0
Victor  Hugo Machado Santos <126088177+VictorHMachadoSantos@users.noreply.github.com>
2024-03-25 20:20:20 -0300

    Add files via upload

---
+++ 2_declarando_variaveis/script.js
@@ -0,0 +1,31 @@
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area);
+var area;
+
+let forma = 'retângulo';
+let altura2 = 5;
+let comprimento2 = 7;
+
+if(forma === 'retângulo'){
+    area = altura2 * comprimento;
+}
+else{
+    area = (altura * comprimento2)
+}
+console.log(area2);
+
+//let area;;
+
+/* A const é uma constante, ou seja, uma vez 
+que a colocamos no programa, não conseguimos alterá-lá. */
+
+const forma3 = 'quadrado';
+const altura3 = 5;
+const comprimento3 = 7;
+
+const area3 = altura3 * comprimento3;
+
+console.log(area3);
